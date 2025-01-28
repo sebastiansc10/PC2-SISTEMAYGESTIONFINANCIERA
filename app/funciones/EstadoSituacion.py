3ae77b56d789d76a76046612c9f8deb7ae814a62
Seteley <tejedasergio2004@gmail.com>
2025-01-28 14:44:06 -0500

    estadosituacioncompleto

--- app/funciones/EstadoSituacion.py
+++ app/funciones/EstadoSituacion.py
@@ -1,5 +1,11 @@
 from app.db_connection import obtener_conexion
 
+import json
+
+import json
+from decimal import Decimal
+
+
 def mayorizar():
     with obtener_conexion() as conn:
         with conn.cursor() as cursor:
@@ -25,6 +31,13 @@ def mayorizar():
             resultados=cursor.fetchall()
     return resultados       
 
+
+
+
+def decimal_default(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)
+    raise TypeError("Type not serializable")
 
 def situacion_activocorriente(fechainicio, fechafin):
     with obtener_conexion() as conn:
@@ -53,5 +66,195 @@ def situacion_activocorriente(fechainicio, fechafin):
                 """,
                 (fechainicio, fechafin)  # Pasamos los parámetros para las fechas
             )
+            # Obtén los resultados como lista de tuplas
             resultados = cursor.fetchall()
-    return resultados
+
+            # Obtén los nombres de las columnas
+            columnas = [desc[0] for desc in cursor.description]
+
+            # Convierte los resultados a una lista de diccionarios
+            resultados_dict = [dict(zip(columnas, fila)) for fila in resultados]
+
+    # Convierte la lista de diccionarios a JSON
+    return json.dumps(resultados_dict, indent=4, ensure_ascii=False, default=decimal_default)
+
+
+def situacion_activonocorriente(fechainicio, fechafin):
+    with obtener_conexion() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """
+                select c.id_cuenta , c.nombre_cuenta,
+                -1 * SUM(CASE 
+                            WHEN t.DH = 'Debe' THEN t.Cantidad  -- Especificamos que `DH` y `Cantidad` provienen de `Transaccion`
+                            WHEN t.DH = 'Haber' THEN -t.Cantidad
+                            ELSE 0 
+                        END) AS Saldo 
+                from transaccion t
+                inner join diario d on d.id_diario = t.id_diario 
+                inner join cuenta c on c.id_cuenta = t.id_cuenta
+                where (c.id_elemento = 3) and (%s <= d.fecha and d.fecha <= %s)
+                group by t.ID_Cuenta, c.nombre_cuenta, c.id_cuenta
+                """,
+                (fechainicio, fechafin)  # Pasamos los parámetros para las fechas
+            )
+            # Obtén los resultados como lista de tuplas
+            resultados = cursor.fetchall()
+
+            # Obtén los nombres de las columnas
+            columnas = [desc[0] for desc in cursor.description]
+
+            # Convierte los resultados a una lista de diccionarios
+            resultados_dict = [dict(zip(columnas, fila)) for fila in resultados]
+
+    # Convierte la lista de diccionarios a JSON
+    return json.dumps(resultados_dict, indent=4, ensure_ascii=False, default=decimal_default)
+
+
+
+def situacion_pasivo(fechainicio, fechafin):
+    with obtener_conexion() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """
+                select c.id_cuenta , c.nombre_cuenta,
+                -1*SUM(CASE 
+                            WHEN t.DH = 'Debe' THEN t.Cantidad  -- Especificamos que `DH` y `Cantidad` provienen de `Transaccion`
+                            WHEN t.DH = 'Haber' THEN -t.Cantidad
+                            ELSE 0 
+                        END) AS Saldo 
+                from transaccion t
+                inner join diario d on d.id_diario = t.id_diario 
+                inner join cuenta c on c.id_cuenta = t.id_cuenta
+                where (c.id_elemento = 4) and (%s <=d.fecha and d.fecha <= %s)
+                group by t.ID_Cuenta, c.nombre_cuenta, c.id_cuenta
+                """,
+                (fechainicio, fechafin)  # Pasamos los parámetros para las fechas
+            )
+            # Obtén los resultados como lista de tuplas
+            resultados = cursor.fetchall()
+
+            # Obtén los nombres de las columnas
+            columnas = [desc[0] for desc in cursor.description]
+
+            # Convierte los resultados a una lista de diccionarios
+            resultados_dict = [dict(zip(columnas, fila)) for fila in resultados]
+
+    # Convierte la lista de diccionarios a JSON
+    return json.dumps(resultados_dict, indent=4, ensure_ascii=False, default=decimal_default)
+
+def situacion_pasivo(fechainicio, fechafin):
+    with obtener_conexion() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """
+                select c.id_cuenta , c.nombre_cuenta,
+                -1*SUM(CASE 
+                            WHEN t.DH = 'Debe' THEN t.Cantidad  -- Especificamos que `DH` y `Cantidad` provienen de `Transaccion`
+                            WHEN t.DH = 'Haber' THEN -t.Cantidad
+                            ELSE 0 
+                        END) AS Saldo 
+                from transaccion t
+                inner join diario d on d.id_diario = t.id_diario 
+                inner join cuenta c on c.id_cuenta = t.id_cuenta
+                where (c.id_elemento = 4) and (%s <=d.fecha and d.fecha <= %s)
+                group by t.ID_Cuenta, c.nombre_cuenta, c.id_cuenta
+                """,
+                (fechainicio, fechafin)  # Pasamos los parámetros para las fechas
+            )
+            # Obtén los resultados como lista de tuplas
+            resultados = cursor.fetchall()
+
+            # Obtén los nombres de las columnas
+            columnas = [desc[0] for desc in cursor.description]
+
+            # Convierte los resultados a una lista de diccionarios
+            resultados_dict = [dict(zip(columnas, fila)) for fila in resultados]
+
+    # Convierte la lista de diccionarios a JSON
+    return json.dumps(resultados_dict, indent=4, ensure_ascii=False, default=decimal_default)
+
+def situacion_patrimonio(fechainicio, fechafin):
+    with obtener_conexion() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """
+                select c.id_cuenta , c.nombre_cuenta,
+                -1 * SUM(CASE 
+                            WHEN t.DH = 'Debe' THEN t.Cantidad  -- Especificamos que `DH` y `Cantidad` provienen de `Transaccion`
+                            WHEN t.DH = 'Haber' THEN -t.Cantidad
+                            ELSE 0 
+                        END) AS Saldo 
+                from transaccion t
+                inner join diario d on d.id_diario = t.id_diario 
+                inner join cuenta c on c.id_cuenta = t.id_cuenta
+                where (c.id_elemento = 5) and (%s <=d.fecha and d.fecha <= %s)
+                group by t.ID_Cuenta, c.nombre_cuenta, c.id_cuenta
+                """,
+                (fechainicio, fechafin)  # Pasamos los parámetros para las fechas
+            )
+            # Obtén los resultados como lista de tuplas
+            resultados = cursor.fetchall()
+
+            # Obtén los nombres de las columnas
+            columnas = [desc[0] for desc in cursor.description]
+
+            # Convierte los resultados a una lista de diccionarios
+            resultados_dict = [dict(zip(columnas, fila)) for fila in resultados]
+
+    # Convierte la lista de diccionarios a JSON
+    return json.dumps(resultados_dict, indent=4, ensure_ascii=False, default=decimal_default)
+
+
+
+def situacion_totalactivocorriente(fechainicio, fechafin):
+    resultados_json = situacion_activocorriente(fechainicio, fechafin)  # obtenemos los resultados como JSON
+    
+    # Convertimos el JSON de vuelta a un diccionario
+    resultados = json.loads(resultados_json)  # Ahora `resultados` es una lista de diccionarios
+    
+    # Calculamos el total
+    total_saldo = sum(fila['saldo'] for fila in resultados)
+    
+    # Si quieres devolver el resultado en formato JSON
+    return json.dumps({"Total_Saldo": total_saldo}, indent=4, ensure_ascii=False, default=decimal_default)
+
+
+
+def situacion_totalactivonocorriente(fechainicio, fechafin):
+    resultados_json = situacion_activonocorriente(fechainicio, fechafin)  # obtenemos los resultados como JSON
+    
+    # Convertimos el JSON de vuelta a un diccionario
+    resultados = json.loads(resultados_json)  # Ahora `resultados` es una lista de diccionarios
+    
+    # Calculamos el total
+    total_saldo = sum(fila['saldo'] for fila in resultados)
+    
+    # Si quieres devolver el resultado en formato JSON
+    return json.dumps({"Total_Saldo": total_saldo}, indent=4, ensure_ascii=False, default=decimal_default)
+
+
+def situacion_totalpasivo(fechainicio, fechafin):
+    resultados_json = situacion_pasivo(fechainicio, fechafin)  # obtenemos los resultados como JSON
+    
+    # Convertimos el JSON de vuelta a un diccionario
+    resultados = json.loads(resultados_json)  # Ahora `resultados` es una lista de diccionarios
+    
+    # Calculamos el total
+    total_saldo = sum(fila['saldo'] for fila in resultados)
+    
+    # Si quieres devolver el resultado en formato JSON
+    return json.dumps({"Total_Saldo": total_saldo}, indent=4, ensure_ascii=False, default=decimal_default)
+
+
+def situacion_totalpatrimonio(fechainicio, fechafin):
+    resultados_json = situacion_patrimonio(fechainicio, fechafin)  # obtenemos los resultados como JSON
+    
+    # Convertimos el JSON de vuelta a un diccionario
+    resultados = json.loads(resultados_json)  # Ahora `resultados` es una lista de diccionarios
+    
+    # Calculamos el total
+    total_saldo = sum(fila['saldo'] for fila in resultados)
+    
+    # Si quieres devolver el resultado en formato JSON
+    return json.dumps({"Total_Saldo": total_saldo}, indent=4, ensure_ascii=False, default=decimal_default)
